ed21547f3c0c52b13dd6ead469eb7df9a3623be6
hwany1458 <hwany1458@empal.com>
2023-11-08 17:25:01 +0900

    Add files via upload

---
+++ InClass2023/1108.js
@@ -0,0 +1,8 @@
+        function over(obj)
+        {
+            obj.src= "./images/peach.jpg";
+        }
+        function out(obj)
+        {
+            obj.src = "./images/apple.jpg";
+        }
